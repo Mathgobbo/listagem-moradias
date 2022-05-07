2f8e07115d4c80df7e9f631fcd491ea0b4fb76cc
Matheus Gobbo Bernardi <matgobbo@hotmail.com>
2022-05-07 16:31:14 -0300

    Mais infos no moradias json

--- components/LivingDetails.js
+++ components/LivingDetails.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { Appbar } from 'react-native-paper';
-import { Text, View, StyleSheet, Image, FlatList } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
 
-export function LivingDetails( {route, navigation} ) {
-  const {moradia} = route.params
+export function LivingDetails({ route, navigation }) {
+  const { moradia } = route.params;
   return (
-    <View><Text>{moradia.nome}</Text></View>
-  )
+    <View>
+      <Text>{moradia.nome}</Text>
+    </View>
+  );
 }
