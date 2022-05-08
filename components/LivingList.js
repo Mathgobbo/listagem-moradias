2ab5613049a2d0e7e72ade42fde157b0f6c2e7a4
Vinicius <vinicius_antunes7@hotmail.com>
2022-05-08 20:05:42 -0300

    Oi 4

--- components/LivingList.js
+++ components/LivingList.js
@@ -7,17 +7,18 @@ export function LivingList({ navigation }) {
       <FlatList
         data={require("../assets/moradias.json")}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => navigation.navigate("Detalhes", { moradia: item })} style={styles.item}>
-            <View style={styles.itemParent}>
+          <TouchableOpacity 
+          onPress={() => navigation.navigate("Detalhes", { moradia: item })} style={styles.item} >
+            <View style={styles.itemParent} >
               <View>
-                <Image source={{uri: item.fotos[0]}} style={styles.imageContainer} />
+                <Image source={{uri: item.fotos[0]}} style={styles.image} />
               </View>
               <View>
-                <Text> {item.nome} </Text>
-                <Text> {item.endereco}</Text>
+                <Text style={styles.infoMoradia}> {item.nome} </Text>
+                <Text style={styles.infoMoradia}> {item.endereco}</Text>
               </View>
-              <View style={styles.priceView}>
-                <Text> R$ {item.valor} </Text>
+              <View style={styles.valorContainer}>
+                <Text style={styles.priceView}> R$ {item.valor} </Text>
               </View>
             </View>
           </TouchableOpacity>
@@ -36,13 +37,21 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
   },
+  infoMoradia: {
+    fontSize: 12,
+  },
   priceView: {
     marginLeft: "auto",
+    fontWeight: "bold",
+    fontSize: 16,
   },
-  imageContainer: {
-    height: 100,
-    width: 100,
+  image: {
+    height: 70,
+    width: 70,
     borderRadius: 5,
-    marginRight: 20,
-  }
+    marginRight: 5,
+  },
+  valorContainer: {
+    marginLeft: "auto",
+  },
 });
